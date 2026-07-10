6554b50a3ba3b953fbc1d4aea8242b47802f4dd9
berezkazzz <maristilinski@gmail.com>
2026-07-10 19:13:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.DRAMIONE_CONFIG = {
-  googleSheetJsonUrl: "https://script.google.com/macros/s/AKfycbzEXA-sLyhAyf7LHJh_SOxG7KQrCVhxWBfnh0YBg4foCyrEBE5YbV-sSD7dk2YsT1N1UQ/exec"
+  googleSheetJsonUrl: "https://script.google.com/macros/s/AKfycbxjDxdx9YoJcHARLMxmI3jJnrcTN1sdWpUMdWFXuaKvDXbuWPSL1pU0viX9v1TNmvxKKQ/exec"
 };
