3aa4d17fca787c5348dfbb4b57d30d4b9dfeca9a
berezkazzz <maristilinski@gmail.com>
2026-07-10 19:05:11 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-// Вставь сюда CSV-ссылку опубликованного листа «Фанфики».
-// Пример:
-// https://docs.google.com/spreadsheets/d/e/2PACX-.../pub?gid=0&single=true&output=csv
 window.DRAMIONE_CONFIG = {
-  googleSheetJsonUrl: "https://script.google.com/macros/s/AKfycbzEXA-sLyhAyf7LHJh_SOxG7KQrCVhxWBfnhOYBg4foCyrEBE5YbV-sSD7dk2YsT1N1UQ/exec"
+  googleSheetJsonUrl: "https://script.google.com/macros/s/AKfycbzEXA-sLyhAyf7LHJh_SOxG7KQrCVhxWBfnh0YBg4foCyrEBE5YbV-sSD7dk2YsT1N1UQ/exec"
 };
